7c63ea9406c130ac02146ab35ade161f1f7f6b37
Owusu-Kwame <90360728+Owusu-Kwame@users.noreply.github.com>
2021-11-03 08:31:43 +0000

    completed login page
    i hace started home page

--- App.js
+++ App.js
@@ -1,21 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import Login from "./app/screens/Login";
+import Home from "./app/screens/Home";
+import ProductDetails from "./app/screens/ProductDescription";
 
 export default function App() {
+  const Stack = createStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Welcome to E-Market </Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Login"
+        screenOptions={{ headerShown: false }}
+      >
+        <Stack.Screen name="Login" component={Login} />
+        {/* <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Productdetails" component={ProductDescription} /> */}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
